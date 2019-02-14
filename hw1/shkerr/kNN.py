c41372ec86fce0f8fa0c7d818752fc4e88a2223e
shkerr <shkerr@wisc.edu>
2019-02-13 18:57:43 -0600

    gives output

--- hw1/shkerr/kNN.py
+++ hw1/shkerr/kNN.py
@@ -2,9 +2,8 @@
 import json
 import numpy as np
 import sys
-import time
-test = "./data/digits_test.json"
-train = "./data/digits_train.json"
+test = "./data/votes_test.json"
+train = "./data/votes_train.json"
 k = 10
 
 #Load the data in
@@ -30,13 +29,13 @@ cat_feat = []
 for feat in range(0,nFeat):
     if metadata[feat][1] == 'numeric':
         sum = np.sum(train.T[feat], axis = 0) #sums all observations for a specific feature
-        mean[feat] = sum/nTrain #Maybe need to do a +1 here?
+        mean[feat] = sum/nTrain 
         sqerror = np.sum((train.T[feat]-mean[feat])**2)
         stddev[feat] = np.sqrt(sqerror/nTrain)
         if stddev[feat] == 0: 
             stddev[feat] = 1
-        train[:][feat] = (train[:][feat] - mean[feat])/stddev[feat] #standardize train set
-        test[:][feat] = (test[:][feat] - mean[feat])/stddev[feat]   #standardize test set
+        #train[:][feat] = (train[:][feat] - mean[feat])/stddev[feat] #standardize train set
+        #test[:][feat] = (test[:][feat] - mean[feat])/stddev[feat]   #standardize test set
         num_feat.append(feat) #Builds a list of the indices of numericFeatures
     else:
         cat_feat.append(feat) #Builds a list of categorical features
@@ -64,17 +63,25 @@ for i in range(0,nTest):
             #Add distance back into distance array
             distance[i] += (cat_diff.astype(int))
     #Now, sort by distance and select k nearest neighbors
-    smallest = np.argsort(distance[i], axis = 0)
+    smallest = np.argsort(distance[i], axis = 0, kind = "mergesort")
     nn[i] = smallest[0:k]
 
-votes = np.zeros((nTest,nLabels), dtype = int)
+
 #Next, I need to let each nn vote:
 for i in range(0,nTest):
-    #Loop through labels
+    #Reset highest votes and nn_label
+    nn_label = []
+    highest_votes = 0
+    #Loop through nn[i] and build label list
+    for neighbor in nn[i]:
+        nn_label.append(train[neighbor][nFeat])
+    #Loop through labels and build votes
     for l in range(0,nLabels):
-        #Loop through nn
-        for neighbor in nn[i]:
-            if train[neighbor][nFeat] == metadata[nFeat][1][l]:
-                votes[i][l] += 1
-        #Print vote total after going through all nn
-    print(votes[i],",")
+        x = metadata[nFeat][1][l] #Pull the l label value, store in x
+        num_votes = nn_label.count(x) #Count number of votes for l label
+        print(num_votes,end=',') #Print number of votes for l label
+        if num_votes > highest_votes:
+            highest_votes = num_votes
+            winner = x
+    print(winner) #After we have counted all votes, print winner and go to new line
+
