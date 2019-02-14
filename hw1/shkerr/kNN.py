521a613c8c545ac17ee996ca13f32112d1e6c8f7
shkerr <shkerr@wisc.edu>
2019-02-13 19:38:06 -0600

    Now executable from bash

--- hw1/shkerr/kNN.py
+++ hw1/shkerr/kNN.py
@@ -2,9 +2,20 @@
 import json
 import numpy as np
 import sys
+import argparse
 test = "./data/votes_test.json"
 train = "./data/votes_train.json"
 k = 10
+
+parser = argparse.ArgumentParser(description='Implement a k-NN algorithm')
+parser.add_argument("-k", type=int, help="Number of nearest neighbors to look for in choose(n,k)")
+parser.add_argument('-train', type = str, help='Train data set path')
+parser.add_argument('-test', type = str, help='Test data set path')
+args = parser.parse_args()
+k = args.k
+train = args.train
+test = args.test
+test = test.replace('\r','') #Removes the carriage return cuz I use windows
 
 #Load the data in
 with open(train,"r") as read_file:
